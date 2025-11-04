5ceac351b12927653db33496e2bc222d457d41ea
Minh Anh Nguyen <anhqtaktb@gmail.com>
2025-11-04 15:49:18 +0700

    Thêm entity Post và cấu trúc cơ sở dữ liệu

---
+++ src/CMSBlog.Core/Domain/Content/Series.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSBlog.Core.Domain.Content
+{
+    [Table("PostTags")]
+    [PrimaryKey(nameof(PostId), nameof(TagId))]
+    public class Series
+    {
+        public Guid PostId { set; get; }
+        public Guid TagId { set; get; }
+    }
+}
---
+++ src/CMSBlog.Core/Domain/Content/Tag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSBlog.Core.Domain.Content
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Slug { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+    }
+}
